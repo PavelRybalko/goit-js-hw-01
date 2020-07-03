693329977a1da5d9a666a7325ae9c5bfe7148aa4
Pavel Rybalko <freedom277f@gmail.com>
2020-07-03 21:52:09 +0300

    fixes

--- js/task-6.js
+++ js/task-6.js
@@ -2,14 +2,13 @@ let input;
 let total = 0;
 
 do {
-	input = prompt('Введите число');
-	const notANumber = isNaN(input);
-	if (notANumber){
-		alert("Было введено не число, попробуйте ещё раз")
-		continue;
-	}
-	total += Number(input);
-}
-while (input !==null);
+  input = prompt("Введите число");
+  const notANumber = isNaN(input);
+  if (notANumber) {
+    alert("Было введено не число, попробуйте ещё раз");
+    continue;
+  }
+  total += +input;
+} while (input !== null);
 
-alert(`Общая сума чисел равна ${total}`)
+alert(`Общая сума чисел равна ${total}`);
